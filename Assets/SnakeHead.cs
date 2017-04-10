e0babc972bd4fb36c94697b5b87134e298659133
anttijankeri <antti.jankeri@gmail.com>
2017-04-10 14:35:17 +0300

    Functional snake tail added

---
+++ Assets/SnakeHead.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeHead : MonoBehaviour {
+	private int health = 500;
+	private float tailDistance = (float) 0.003;
+	private float speed;
+	private bool moving;
+
+	private List<SnakeTail> tailList;
+	private GameObject snakeTailPrefab;
+	private PlayerController playerController;
+
+	private Vector3 prevPosition;
+
+	private GameObject AddTailPiece (GameObject following)
+	{
+		GameObject newTail = Instantiate (snakeTailPrefab, transform.position, transform.rotation);
+		SnakeTail newScript = newTail.GetComponent<SnakeTail> ();
+		newScript.FollowedObject = following;
+		newScript.SnakeHeadScript = this;
+		newScript.FollowDistance = tailDistance;
+		tailList.Add (newScript);
+		return newTail;
+	}
+
+	public bool Moving
+	{
+		get
+		{
+			return this.moving;
+		}
+		set
+		{
+			this.moving = value;
+		}
+	}
+
+	public float Speed
+	{
+		get
+		{
+			return this.speed;
+		}
+		set
+		{
+			this.speed = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		playerController = this.gameObject.GetComponent<PlayerController> ();
+		snakeTailPrefab = Resources.Load ("SnakeTail") as GameObject;
+		prevPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+
+		GameObject following = this.gameObject;
+		tailList = new List<SnakeTail> ();
+
+		for (int i = 0; i < health; i++)
+		{
+			following = AddTailPiece (following);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Speed = playerController.GetRealSpeed ();
+		Moving = (prevPosition != transform.position) ? true : false;
+		prevPosition = transform.position;
+	}
+}
---
+++ Assets/SnakeTail.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeTail : MonoBehaviour {
+	private GameObject followedObject;
+	private SnakeHead snakeHead;
+	private float followDistance;
+
+	public GameObject FollowedObject
+	{
+		get
+		{
+			return this.followedObject;
+		}
+		set
+		{
+			this.followedObject = value;
+		}
+	}
+
+	public SnakeHead SnakeHeadScript
+	{
+		get
+		{
+			return this.snakeHead;
+		}
+		set
+		{
+			this.snakeHead = value;
+		}
+	}
+
+	public float FollowDistance
+	{
+		get
+		{
+			return this.followDistance;
+		}
+		set
+		{
+			this.followDistance = value;
+		}
+	}
+
+	private void FollowTarget ()
+	{
+		Vector3 posDifference = new Vector3 (0, 0, 0);
+		posDifference = FollowedObject.transform.position - transform.position;
+		float len = posDifference.magnitude;
+		if (SnakeHeadScript.Moving || len > FollowDistance)
+		{
+			float radAngle = Mathf.Atan2 (posDifference.y, posDifference.x);
+
+			transform.eulerAngles = new Vector3 (0, 0, radAngle * Mathf.Rad2Deg);
+
+			float frameSpeed = Mathf.Min (len - FollowDistance, SnakeHeadScript.Speed * Time.deltaTime);
+
+			transform.position += new Vector3 (Mathf.Cos (radAngle) * frameSpeed, Mathf.Sin (radAngle) * frameSpeed, 0);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FollowTarget ();
+	}
+}
