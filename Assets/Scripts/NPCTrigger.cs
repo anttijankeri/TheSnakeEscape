296da71667d73fc02bb2766224f22ed56c85db54
anttijankeri <antti.jankeri@gmail.com>
2017-04-26 13:09:37 +0300

    added npc triggers

--- Assets/Scripts/NPCTrigger.cs
+++ Assets/Scripts/NPCTrigger.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+/// <summary>
+/// Triggers the NPC to start moving on their path when the player collides
+/// </summary>
 public class NPCTrigger : MonoBehaviour {
+	// the object name of the npc to trigger
+	[SerializeField]
+	private string triggeredObjectName;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	/// <summary>
+	/// When triggering with the player
+	/// </summary>
+	/// <param name="other">Other.</param>
+	void OnTriggerEnter (Collider other)
+	{
+		// check if the trigger is the player
+		if (other.gameObject.name == "SnakeHead")
+		{
+			// trigger the npc's movement
+			GameObject.Find (triggeredObjectName).GetComponent<NPCPathing> ().moving = true;
+
+			// self destruct
+			GameObject.Destroy (gameObject);
+		}
 	}
 }
