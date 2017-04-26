43ad9a3a3567a12f4ea5dbbb177d60bef7c57e0c
anttijankeri <antti.jankeri@gmail.com>
2017-04-26 11:00:58 +0300

    reeeeeeeeee

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+/// <summary>
+/// A single waypoint with a position relative to the gameobject
+/// </summary>
+public class Waypoint {
+	[SerializeField]
+	public Vector3 position;
+}
