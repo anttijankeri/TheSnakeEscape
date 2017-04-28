68d74fdbd7859cf0eea747335733b95230de2a1b
anttijankeri <antti.jankeri@gmail.com>
2017-04-28 13:41:08 +0300

    gameworld 2 complete

---
+++ Assets/Scripts/TimeTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Trigger for starting the countdown timer
+/// </summary>
+public class TimeTrigger : MonoBehaviour {
+
+	/// <summary>
+	/// Starts the timer if the player collides with it after the room teleporter is open
+	/// </summary>
+	/// <param name="other">Other.</param>
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.name == "SnakeHead")
+		{
+			if (GameObject.Find("RoomTeleporter").GetComponent<RoomTeleporter> ().Open)
+			{
+				GameController.StartTimer ();
+			}
+		}
+	}
+}
