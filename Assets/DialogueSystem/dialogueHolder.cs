eea72ad3817a91876f9fa11127be89e1966abfb5
anttijankeri <antti.jankeri@gmail.com>
2017-05-05 13:57:40 +0300

    magic

--- Assets/DialogueSystem/dialogueHolder.cs
+++ Assets/DialogueSystem/dialogueHolder.cs
@@ -29,12 +29,28 @@ public class dialogueHolder : MonoBehaviour {
 	// bool resets to false when exiting the dialogue zone.
 	private bool repeatCD = false;
 
+	// if currently standing on the holder and its ready to blow when the puzzles are all solved
+	private bool waiting = false;
+
     /// <summary>
     /// Loads dialogueManager in the start of every scene
     /// </summary>
     // Use this for initialization
     void Start () {
         dMAn = FindObjectOfType<dialogueManager>();
+	}
+
+	void Update ()
+	{
+		// if waiting for the puzzles to be solved and they were solved
+		if (waiting && !dialogueUsed && GameObject.Find("RoomTeleporter").GetComponent<RoomTeleporter> ().Open)
+		{
+			dMAn.dialogLines = dialogueLines;
+			dMAn.currentLine = 0;
+			dMAn.ShowDialogue();
+			dialogueUsed = true;
+			repeatCD = true;
+		}
 	}
 
     /// <summary>
@@ -55,6 +71,7 @@ public class dialogueHolder : MonoBehaviour {
 				{
 					if (!GameObject.Find("RoomTeleporter").GetComponent<RoomTeleporter> ().Open)
 					{
+						waiting = true;
 						return;
 					}
 				}
@@ -93,6 +110,9 @@ public class dialogueHolder : MonoBehaviour {
 		{
 			// reset the repeat cooldown
 			repeatCD = false;
+
+			// not colliding with the holder anymore, thus not ready to blow
+			waiting = false;
 		}
 	}
 }
